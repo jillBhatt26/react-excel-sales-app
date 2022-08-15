9989898c0da53e779742749dee8d444c8947ec48
jill bhatt <bhattjill@gmail.com>
2022-08-15 12:11:12 +0530

    Added navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+const Nav = () => {
+    return (
+        <nav className="bg-bgNav text-white ">
+            <h1 className="container mx-auto pb-3 pt-1.5 pl-5 text-3xl md:text-4xl">
+                Sales App
+            </h1>
+        </nav>
+    );
+};
+
+export default Nav;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                bgNav: '#2f3542',
+                bgBody: '#dfe6e9',
+                bgFormBtnSubmit: '#2ed573',
+                bgFormBtnClear: '#ff4757'
+            }
+        }
     },
     plugins: []
 };
